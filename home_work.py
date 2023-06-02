b1dad5ab61cfa0642e28323a4a0896f88823c6a9
Daria <darina.vaxrusheva@bk.ru>
2023-06-02 17:15:43 +0400

    lesson1

---
+++ home_work.py
@@ -0,0 +1,38 @@
+# Задача № 8
+
+# count = int(input('Введите кол-во рядов елки: '))
+# for i in range(1, count + 1):
+#     a = " " * (count - i)
+#     b = "*" * (2 * i - 1)
+#     print(a + b)
+
+# #  Задача № 9
+#
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(i * j, end=' ')
+#     print()
+
+# Задача № 2
+
+# a = int(input('Введите длину первой стороны: '))
+# b = int(input('Введите длину второй стороны: '))
+# c = int(input('Введите длину третьей стороны: '))
+#
+# num1 = a + b
+# num2 = b + c
+# num3 = a + c
+#
+# if (num1 < c or num2 < a or num3 < b):
+#     print('Такого треугольника не существует!')
+# else:
+#     if (a == b and b == c and a == c):
+#         print("Треугольник равносторонний")
+#     elif (a == b or b == c or a == c):
+#         print("Треугольник равнобедренный ")
+#     else:
+#         print("Треугольник разносторонний  ")
+
+
+
+
---
+++ task_2.py
@@ -0,0 +1,19 @@
+# import math
+#
+# print('5x**2 - 10x - 400 = 0')
+# dis = 10**2 - 4 * 5 * 400
+# print('дискриминант', dis )
+# if dis > 0:
+#    x1 = (-10 + math.sqrt(dis)) / (2*5)
+#    x2 = (-10 - math.sqrt(dis)) / (2*5)
+#    print('x1 = ', x1, 'x2 = ', x2)
+# elif dis == 0:
+#     x = -400 / (2 * 5)
+#     print(x)
+# else:
+#     print('корней нет')
+
+# Задача №5
+n = int(input('Введите число n '))
+e = int(input('Введите число e '))
+

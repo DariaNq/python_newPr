a4c40bb5af9d10ecbecf0130ba1a0d7531250cb5
Daria <darina.vaxrusheva@bk.ru>
2023-06-07 20:13:11 +0400

    home

---
+++ home_work2.py
@@ -0,0 +1,35 @@
+# Задача № 1
+# Напишите программу, которая получает целое число и возвращает его шестнадцатеричное
+# строковое представление. Функцию hex используйте для проверки своего результата
+
+#
+# if __name__ == '__main__':
+#     i = int(input('введите целое число: '))
+#
+#     h = (format(i, '#x'))  # Использование format()
+#     print(h)  # ('0xfff')
+
+
+
+# Задача № 2
+# Напишите программу, которая принимает две строки вида “a/b” — дробь с числителем и знаменателем.
+# Программа должна возвращать сумму и *произведение дробей. Для проверки своего кода используйте модуль fractions.
+
+# import fractions
+#
+# number1: int = int(input("Введите числитель первой дроби "))
+# number2: int = int(input("Введите знаменатель первой дроби "))
+# number3: int = int(input("Введите числитель второй дроби "))
+# number4: int = int(input("Введите знаменатель второй дроби "))
+#
+# result1 = (number1 / number2) + (number3 / number4)
+# result2 = (number1 / number2) * (number3 / number4)
+# print(result1)
+# print(result2)
+# firstfraction = fractions.Fraction(number1, number2)
+# secondfraction = fractions.Fraction(number3, number4)
+# result3 = firstfraction + secondfraction
+# result4 = firstfraction * secondfraction
+# print(result3)
+# print(result4)
+
